6cb8253a581428e25b134c3f7666590bb780245f
Sonsa <13855537+Sonsa04@users.noreply.github.com>
2020-10-05 17:38:13 +0800

    Add files via upload

---
+++ data/mobile.js
@@ -0,0 +1,56 @@
+var mobile = [
+	{
+        "name": "iPlay Donation",
+        "description": "Donation for Minecraft Bedrock Edition",
+        "type": 'Client Project',
+        "thumbnail": "../images/mobile/iplaydonation/thumbnail.png",
+        "videos": [
+			"https://www.youtube.com/embed/Uiqtjyfhg2M",
+        ],
+        "images": [
+            "../images/mobile/iplaydonation/1.png",
+			"../images/mobile/iplaydonation/2.png",
+			"../images/mobile/iplaydonation/3.png",
+			"../images/mobile/iplaydonation/4.png",
+        ],
+    },
+	{
+        "name": "iPlay Hosting",
+        "description": "Hosting for Minecraft Bedrock Edition",
+        "type": 'Client Project',
+        "thumbnail": "../images/mobile/iplayhosting/thumbnail.png",
+        "videos": [
+			"https://www.youtube.com/embed/NMgZifIV2E0",
+        ],
+        "images": [
+            "../images/mobile/iplayhosting/1.png",
+			"../images/mobile/iplayhosting/2.png",
+			"../images/mobile/iplayhosting/3.png",
+        ],
+    },
+	{
+        "name": "Circle Hit",
+        "description": "Simple hyper casual game",
+        "type": 'Personal Project',
+        "thumbnail": "../images/mobile/circlehit/thumbnail.png",
+        "videos": [
+        ],
+        "images": [
+            "../images/mobile/circlehit/1.png",
+			"../images/mobile/circlehit/2.png",
+        ],
+    },
+    {
+        "name": "Puzzle Match",
+        "description": "Simple hyper casual game",
+        "type": 'Personal Project',
+        "thumbnail": "../images/mobile/puzzlematch/thumbnail.png",
+        "videos": [
+        ],
+        "images": [
+            "../images/mobile/puzzlematch/1.png",
+			"../images/mobile/puzzlematch/2.png",
+			"../images/mobile/puzzlematch/3.png",
+        ],
+    },
+]
